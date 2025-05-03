dc73a9cf914017bd9808291d18f8839f2b5ef8d8
eykd <david@worldsenoughstudios.com>
2025-05-03 06:29:58 -0700

    chore: rename test files to match new module names

--- src/prosemark/domain/node.py
+++
@@ -1,5 +0,0 @@
-"""This module is deprecated. Import from prosemark.domain.nodes instead."""
-
-from prosemark.domain.nodes import Node
-
-__all__ = ['Node']
--- src/prosemark/domain/project.py
+++
@@ -1,5 +0,0 @@
-"""This module is deprecated. Import from prosemark.domain.projects instead."""
-
-from prosemark.domain.projects import Project
-
-__all__ = ['Project']
