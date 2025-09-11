4ea0f466eb635e669228b1e2e118395c40f6ce6b
eykd <david@worldsenoughstudios.com>
2025-09-11 09:26:23 -0700

    feat: enable unsafe fixes for linters

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -7,7 +7,7 @@ source .env.test
 set +a
 
 ## # Automatically reformat code, but ignore breakpoint() and commented code:
-uv run ruff check --fix --ignore T100  --ignore ERA001
+uv run ruff check --fix --unsafe-fixes --ignore T100  --ignore ERA001
 uv run ruff format
 uv run mypy src tests
 uv run  --env-file=.env.test \
